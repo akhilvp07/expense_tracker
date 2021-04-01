9168c9ad75cb21fc516e486f63acc3cabed26e6d
Akhil <avalsamrithan@rbbn.com>
2021-04-01 17:11:43 +0530

    Added rough sketch of the app
    Fix: This is an outline of the app

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import './transaction.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
@@ -7,20 +8,64 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Expense Tracker App',
+      debugShowCheckedModeBanner: false,
       home: MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatelessWidget {
+  final List<Transaction> transactions = [
+    Transaction(
+      id: 't1',
+      title: 'Petrol',
+      amount: 995.5,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 't2',
+      title: 'Food',
+      amount: 455.44,
+      date: DateTime.now(),
+    ),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Expense Tracker'),
       ),
-      body: Center(
-        child: Text('Windgets here!'),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          Container(
+            width: double.infinity,
+            child: Card(
+              color: Colors.blue,
+              child: Text('Chart!'),
+            ),
+          ),
+          Column(
+              children: transactions.map((tx) {
+            return Card(
+              child: Row(
+                children: <Widget>[
+                  Container(
+                    child: Text(tx.amount.toString()),
+                  ),
+                  Column(
+                    children: <Widget>[
+                      Text(tx.title),
+                      Text(tx.date.toString()),
+                    ],
+                  ),
+                ],
+              ),
+            );
+          }).toList()),
+        ],
       ),
     );
   }
