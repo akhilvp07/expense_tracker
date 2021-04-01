8758047238f4261232a04e93255660e27e1ab21a
Akhil <avalsamrithan@rbbn.com>
2021-04-01 22:35:34 +0530

    More formatting to the transactions
    Fix: Formatting and cleaning up pf the transactions card.

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
-import './transaction.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+import './transaction.dart';
 
 void main() => runApp(MyApp());
 
@@ -19,7 +21,7 @@ class MyHomePage extends StatelessWidget {
     Transaction(
       id: 't1',
       title: 'Petrol',
-      amount: 995.5,
+      amount: 995.50,
       date: DateTime.now(),
     ),
     Transaction(
@@ -49,16 +51,51 @@ class MyHomePage extends StatelessWidget {
           ),
           Column(
               children: transactions.map((tx) {
+            //Card for transactions
             return Card(
               child: Row(
                 children: <Widget>[
+                  //Container to display amount
                   Container(
-                    child: Text(tx.amount.toString()),
+                    margin: EdgeInsets.symmetric(
+                      horizontal: 15,
+                      vertical: 10,
+                    ),
+                    child: Text(
+                      '₹ ${tx.amount.toStringAsFixed(2)}',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20,
+                        color: Colors.deepOrange,
+                      ),
+                    ),
+                    decoration: BoxDecoration(
+                      border: Border.all(
+                        color: Colors.deepOrange,
+                        width: 2,
+                      ),
+                    ),
+                    padding: EdgeInsets.all(10),
                   ),
+                  //Column for the title and date of transaction
                   Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
                     children: <Widget>[
-                      Text(tx.title),
-                      Text(tx.date.toString()),
+                      Text(
+                        tx.title,
+                        style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      Text(
+                        DateFormat('dd-MMM-yyyy hh:mm a').format(tx.date),
+                        style: TextStyle(
+                          fontSize: 15,
+                          fontWeight: FontWeight.normal,
+                          color: Colors.grey,
+                        ),
+                      ),
                     ],
                   ),
                 ],
